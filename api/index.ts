dab5b597a81cd37878124bd215e4701cba121e36
samesaeed456-droid <samesaeed456@gmail.com>
2026-05-14 21:16:08 +0300

    feat: Simplify Vercel API handler
    Removes unnecessary boilerplate from the Vercel serverless function handler, directly exporting the main app instance. Updates `vercel.json` to correctly include files for the build.

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,4 @@ import app from '../server';
 
 console.log("[Vercel Function] Loaded api/index.ts");
 
-export default (req: any, res: any) => {
-  console.log(`[Vercel Function] Handling ${req.method} ${req.url}`);
-  return app(req, res);
-};
+export default app;
